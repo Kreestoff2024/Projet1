1658864172f18b025f194fc8e8e3a255c45f07d3
Christophe Lefrançois <christophe.lefrancois2024@campus-eni.fr>
2025-02-16 16:11:06 +0100

    ajout fichier locust

---
+++ locustfile.py
@@ -0,0 +1,13 @@
+from locust import HttpUser, task
+
+class ProjectPerfTest(HttpUser):
+	@task
+	def home(self):
+		response = self.client.get("/home")
+		
+		
+		
+	@task(3)
+	def login(self):
+	 response = self.client.get("/user/login")
+		
